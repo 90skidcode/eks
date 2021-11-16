7f02a19406a338b1bb15f63848e6a356602a2ff3
Your Name90skidcode <90skidcode@gmail.com>
2021-11-16 22:57:57 +0530

    update

--- asst/js/menu.js
+++ asst/js/menu.js
@@ -17,54 +17,56 @@ var customerUrlid = url.searchParams.get("customer");
 $(document).ready(function() {
     $('body').prepend(html);
     user = JSON.parse(localStorage.getItem('user'));
-    let menuDom = ` <li class="nav-item  d-lg-block"><a href="../../pages/dashboard/dashboard.html" class="nav-link nav-link-style"><i class="ficon"
-                        data-feather="home"></i></a>
-                    </li>
-                        <li class="nav-item  d-lg-block"><a class="nav-link nav-link-style share-link"><i class="ficon"
-                        data-feather="share-2"></i></a>
-                        </li>
-                        <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" id="dropdown-setting" href="#" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <i class="ficon" data-feather="settings"></i>
-                        </a>
-                        <div class="dropdown-menu dropdown-menu-end setting-menu" aria-labelledby="dropdown-setting">
-                        
-                        </div>
-                    </li>
-                        <li class="nav-item  d-lg-block"><a href="../../pages/add/add.html" class="nav-link nav-link-style"><i class="ficon"
-                        data-feather="plus"></i></a>
-                        </li>
-                        <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" id="dropdown-flag" href="#" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <i class="ficon" data-feather="grid"></i>
-                        </a>
-                        <div class="dropdown-menu dropdown-menu-end company-menu" aria-labelledby="dropdown-flag">
-                        
-                        </div>
-                    </li>
-                        <li class="nav-item d-none log-outs">
-                            <a class="dropdown-item" href="../../index.html"><i class="ficon" data-feather="log-out"></i></a>
-                        </li>
-                    <li class="nav-item dropdown dropdown-language">
-                        <a class="nav-link dropdown-toggle" id="dropdown-flag" href="#" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <span class="selected-language"></span></a>
-                        <div class="dropdown-menu dropdown-menu-end company-list" aria-labelledby="dropdown-flag">
-                        
-                        </div>
-                    </li>
-                    
-                    <li class="nav-item dropdown dropdown-user">
-                        <a class="nav-link dropdown-toggle dropdown-user-link" id="dropdown-user" href="#" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <div class="user-nav d-sm-flex "><span class="user-name fw-bolder">1000120</span><span class="user-status">Admin</span></div>
-                            <span class="avatar">
-                            <span 
-                            class="avatar-status-online"></span></span>
-                        </a>
-                        <div class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdown-user">
-                            <a class="dropdown-item" href="../../index.html"><i class="me-50"
-                                    data-feather="power"></i> Logout</a>
-                        </div>
-                    </li>`;
+    let menuDom = ` <li class="nav-item  d-lg-block">
+    <a href="../../pages/dashboard/dashboard.html" class="nav-link nav-link-style">
+        <i class="fi fi-home"></i></a>
+</li>
+<li class="nav-item  d-lg-block">
+    <a class="nav-link nav-link-style share-link">
+        <i class="fi fi-share"></i></a>
+</li>
+<li class="nav-item dropdown">
+    <a class="nav-link dropdown-toggle" id="dropdown-setting" href="#" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    <i class="fi fi-player-settings"></i>
+    </a>
+    <div class="dropdown-menu dropdown-menu-end setting-menu" aria-labelledby="dropdown-setting">
+
+    </div>
+</li>
+<li class="nav-item  d-lg-block">
+    <a href="../../pages/add/add.html" class="nav-link nav-link-style">
+    <i class="fi fi-plus-a"></i>
+</li>
+<li class="nav-item dropdown">
+    <a class="nav-link dropdown-toggle" id="dropdown-flag" href="#" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    <i class="fi fi-nav-icon-a"></i>
+    </a>
+    <div class="dropdown-menu dropdown-menu-end company-menu" aria-labelledby="dropdown-flag">
+
+    </div>
+</li>
+<li class="nav-item d-none log-outs">
+    <a class="dropdown-item" href="../../index.html">
+    <i class="fi fi-power"></i></a>
+</li>
+<li class="nav-item dropdown dropdown-language">
+    <a class="nav-link dropdown-toggle" id="dropdown-flag" href="#" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        <span class="selected-language"></span></a>
+    <div class="dropdown-menu dropdown-menu-end company-list" aria-labelledby="dropdown-flag">
+    </div>
+</li>
+
+<li class="nav-item dropdown dropdown-user">
+    <a class="nav-link dropdown-toggle dropdown-user-link" id="dropdown-user" href="#" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        <div class="user-nav d-sm-flex "><span class="user-name fw-bolder">1000120</span><span class="user-status">Admin</span></div>
+        <span class="avatar">
+            <span class="avatar-status-online"></span></span>
+    </a>
+    <div class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdown-user">
+        <a class="dropdown-item" href="../../index.html">
+        <i class="fi fi-power"></i> Logout</a>
+    </div>
+</li>`;
 
 
 
@@ -72,12 +74,12 @@ $(document).ready(function() {
         $('body').addClass('customer-view');
         localStorage.removeItem("user");
         localStorage.setItem('customer-id', customerUrlid);
-        menuDom = ` <li class="nav-item  d-lg-block"><a href="../../pages/dashboard/dashboard.html?customer=${customerUrlid}" class="nav-link nav-link-style"><i class="ficon"
-                        data-feather="home"></i></a>
+        menuDom = ` <li class="nav-item  d-lg-block"><a href="../../pages/dashboard/dashboard.html?customer=${customerUrlid}" 
+        class="nav-link nav-link-style"><i class="fi fi-home"></i></a>
                     </li>
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" id="dropdown-flag" href="#" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <i class="ficon" data-feather="grid"></i>
+                        <i class="fi fi-nav-icon-a"></i>
                         </a>
                         <div class="dropdown-menu dropdown-menu-end company-menu" aria-labelledby="dropdown-flag">
                         
@@ -85,24 +87,25 @@ $(document).ready(function() {
                     </li>`
     } else {
         if (user[0].user_type == '1') {
-            menuDom = ` <li class="nav-item  d-lg-block"><a href="../../pages/dashboard/dashboard.html" class="nav-link nav-link-style"><i class="ficon"
-            data-feather="home"></i></a>
+            menuDom = ` <li class="nav-item  d-lg-block"><a href="../../pages/dashboard/dashboard.html" class="nav-link nav-link-style">
+            <i class="fi fi-home"></i></a>
         </li>
-            <li class="nav-item  d-lg-block"><a class="nav-link nav-link-style share-link"><i class="ficon"
-            data-feather="share-2"></i></a>
+            <li class="nav-item  d-lg-block"><a class="nav-link nav-link-style share-link"><i class="fi fi-share"></i></a>
             </li>
-            <li class="nav-item  d-lg-block"><a href="../../pages/add/add.html" class="nav-link nav-link-style"><i class="ficon"
-            data-feather="plus"></i></a>
+            <li class="nav-item  d-lg-block"><a href="../../pages/add/add.html" class="nav-link nav-link-style"><i class="fi fi-plus-a"></i></a>
             </li>
             <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" id="dropdown-flag" href="#" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    <i class="ficon" data-feather="grid"></i>
+                <i class="fi fi-nav-icon-a"></i>
                 </a>
                 <div class="dropdown-menu dropdown-menu-end company-menu" aria-labelledby="dropdown-flag">
                 
                 </div>
             </li>
-    
+            <li class="nav-item d-none log-outs">
+            <a class="dropdown-item" href="../../index.html">
+            <i class="fi fi-power"></i></a>
+        </li>
         <li class="nav-item dropdown dropdown-language">
             <a class="nav-link dropdown-toggle" id="dropdown-flag" href="#" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 <span class="selected-language"></span></a>
@@ -110,7 +113,7 @@ $(document).ready(function() {
             
             </div>
         </li>
-    
+       
         <li class="nav-item dropdown dropdown-user">
             <a class="nav-link dropdown-toggle dropdown-user-link" id="dropdown-user" href="#" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 <div class="user-nav d-sm-flex "><span class="user-name fw-bolder">1000120</span><span class="user-status">Admin</span></div>
@@ -119,8 +122,7 @@ $(document).ready(function() {
                 class="avatar-status-online"></span></span>
             </a>
             <div class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdown-user">
-                <a class="dropdown-item" href="../../index.html"><i class="me-50"
-                        data-feather="power"></i> Logout</a>
+                <a class="dropdown-item" href="../../index.html"><i class="fi fi-power"></i> Logout</a>
             </div>
         </li>`;
         }
@@ -226,7 +228,10 @@ function listform(params) {
         menu += `<a  href='../table/table.html?id=${e.master_table_id}' class="dropdown-item">${e.master_form_name}</a>`;
     });
     $('.company-menu').html(menu);
-    getreadyDashbord();
+    try {
+        getreadyDashbord();
+    } catch (e) {}
+
 }
 
 $(document).on('click', '.share-link', function() {
