946917f98bce627defd4dfca39bb8a163811c20d
Your Name90skidcode <90skidcode@gmail.com>
2021-12-06 10:29:05 +0530

    up

--- pages/dashboard/dashboard.js
+++ pages/dashboard/dashboard.js
@@ -16,6 +16,9 @@ function getreadyDashbord() {
 let up = `<svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trending-up text-success font-medium-1"><polyline points="23 6 13.5 15.5 8.5 10.5 1 18"></polyline><polyline points="17 6 23 6 23 12"></polyline></svg>`;
 let down = `<svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trending-down text-danger font-medium-1"><polyline points="23 18 13.5 8.5 8.5 13.5 1 6"></polyline><polyline points="17 18 23 18 23 12"></polyline></svg>`;
 
+up = '';
+down = '';
+
 function dashboard(params) {
 
     let cu = params.result.current_month;
@@ -137,13 +140,12 @@ function dashboard(params) {
 
                         <td>
                         <div class="d-flex align-items-center">
-                            <span class="fw-bolder mb-25">${setZeroForUndefined(0)}</span>
-                            ${ up}
+                            <span class="fw-bolder mb-25">${setZeroForUndefined(cu.coolant_concentration)}</span>
                         </div>
                     </td>
                     <td>
                         <div class="d-flex align-items-center">
-                            <span class="fw-bolder me-1">${setZeroForUndefined(0)}</span>
+                            <span class="fw-bolder me-1">${setZeroForUndefined(pre.coolant_concentration)}</span>
                         </div>
                     </td>
                     </tr>
@@ -156,13 +158,13 @@ function dashboard(params) {
 
                         <td>
                         <div class="d-flex align-items-center">
-                            <span class="fw-bolder mb-25">${setZeroForUndefined(0)}</span>
+                            <span class="fw-bolder mb-25">${setZeroForUndefined(cu.oil_level_abnormality)}</span>
                             ${ up }
                         </div>
                     </td>
                     <td>
                         <div class="d-flex align-items-center">
-                            <span class="fw-bolder me-1">${setZeroForUndefined(0)}</span>
+                            <span class="fw-bolder me-1">${setZeroForUndefined(pre.oil_level_abnormality)}</span>
                         </div>
                     </td>
                     </tr>
@@ -175,13 +177,13 @@ function dashboard(params) {
 
                         <td>
                         <div class="d-flex align-items-center">
-                            <span class="fw-bolder mb-25">${setZeroForUndefined(0)}</span>
+                            <span class="fw-bolder mb-25">${setZeroForUndefined(cu.leak)}</span>
                             ${ up }
                         </div>
                     </td>
                     <td>
                         <div class="d-flex align-items-center">
-                            <span class="fw-bolder me-1">${setZeroForUndefined(0)}</span>
+                            <span class="fw-bolder me-1">${setZeroForUndefined(pre.leak)}</span>
                         </div>
                     </td>
                     </tr>
